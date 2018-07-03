7aab9ddab58116663b852ec0a2b158cc3cd4037f
vizuleanu <paliciucdan99@gmail.com>
2018-07-03 22:05:58 +0300

    Install service worker

--- sw.js
+++ sw.js
@@ -1,1 +1,27 @@
-console.log("hekko");
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        "/",
+        "index.html",  
+        "restaurant.html",
+        "css/styles.css",
+        "data/restaurants.json",
+        "js/dbhelper.js",
+        "js/main.js",
+        "js/restaurant_info.js",
+        "sw_registration.js",
+        "img/1.jpg",
+        "img/2.jpg",
+        "img/3.jpg",
+        "img/4.jpg",
+        "img/5.jpg",
+        "img/6.jpg",
+        "img/7.jpg",
+        "img/8.jpg",
+        "img/9.jpg",
+        "img/10.jpg"
+      ]);
+    })
+  );
+});
