41830f8d9d9bec29413397e6e44cd3a2387363d3
vizuleanu <paliciucdan99@gmail.com>
2018-07-04 20:27:56 +0300

    Activate service worker

--- sw.js
+++ sw.js
@@ -25,3 +25,54 @@ self.addEventListener('install', function(event) {
     })
   );
 });
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v2').then(function(cache) {
+      return cache.addAll([
+        "/",
+        "index.html",  
+        "restaurant.html",
+        "css/styles.css",
+        "data/restaurants.json",
+        "js/dbhelper.js",
+        "js/main.js",
+        "js/restaurant_info.js",
+        "sw_registration.js",
+        "img/1.jpg",
+        "img/2.jpg",
+        "img/3.jpg",
+        "img/4.jpg",
+        "img/5.jpg",
+        "img/6.jpg",
+        "img/7.jpg",
+        "img/8.jpg",
+        "img/9.jpg",
+        "img/10.jpg"
+        // include other new resources for the new version...
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  var cacheWhitelist = ['v2'];
+
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
--- sw_registration.js
+++ sw_registration.js
@@ -1,10 +1,12 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js')
-  .then(function(reg) {
-    // registration successfull
-    console.log('Registration succeeded. Scope is ' + reg.scope);
-  }).catch(function(error) {
-    // registration failed
-    console.log('Registration failed with ' + error);
-  });
-}
+document.addEventListener("DOMContentLoaded", function(event) {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js')
+    .then(function(reg) {
+      // registration successfull
+      console.log('Registration succeeded. Scope is ' + reg.scope);
+    }).catch(function(error) {
+      // registration failed
+      console.log('Registration failed with ' + error);
+    });
+  }
+});
